68555c48d4d453d1cdb4aff481d10cce5ed9c267
julienDeveaux <julien.deveaux@hotmail.com>
2022-09-29 22:04:26 +0200

    feat: add user modifications

--- routes/index.js
+++ routes/index.js
@@ -71,6 +71,41 @@ router.get('/logout', function(req, res) {
   req.app.set('user', null);
 });
 
+router.get('/modifyUser', function(req, res) {
+  res.render('modifyUser', {username : req.app.get("user"), role : req.app.get("role")})
+})
+
+router.post('/modifyUser', function(req, res) {
+  Account.find({username : req.app.get('user')}).remove().exec();
+  Account.register(new Account({ username : req.body.username , type : (req.body.type === 'true')}), req.body.password, function(err, account) {
+    if (err) {
+      return res.render('modifyUser', { error : err.message });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      req.session.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        let role = req.app.get('role');
+        if(role === null && req.user !== undefined)
+          role = Account.find({username: req.user}).get("type");
+        if(role) {
+          role = "Utilisateur";
+          req.app.set('role', role);
+        } else {
+          role = "Agent immobilier";
+          req.app.set('role', role);
+        }
+        if(req.user !== undefined) {
+          req.app.set('user', req.user.username);
+        }
+        res.redirect('/');
+      });
+    });
+  });
+})
+
 router.get('/ping', function(req, res){
     res.status(200).send("pong!");
 });
