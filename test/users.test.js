d54b7f0bf3b32c8b82e185812e7d7569566b75e5
Dim145 <dimitri.bleach@gmail.com>
2022-10-10 18:55:28 +0200

    feat: add missing users tests (get routes & error cases)

--- test/users.test.js
+++ test/users.test.js
@@ -15,6 +15,11 @@ describe('Users tests', function() {
     server.get('/').expect(/Immobiliator/, done);
   });
 
+  it('get register page', (done) =>
+  {
+    server.get('/users/register').expect(/Page de création de compte/).end(done);
+  });
+
   it('create one test user', async () =>
   {
     await server.post('/users/register').send({
@@ -24,6 +29,28 @@ describe('Users tests', function() {
     }).expect(302);
 
     return expect((await accounts.findOne({})).username).toBe('test');
+  });
+
+  it('test register already exist user', async () =>
+  {
+    await server.post('/users/register').send({
+      username: 'test',
+      type: true, // classic user
+      password: 'test'
+    }).expect(/text-danger/);
+  });
+
+  it('get login page', (done) =>
+  {
+    server.get('/users/login').expect(/Page de connexion/).end(done);
+  });
+
+  it('login with bad username', (done) =>
+  {
+    server.post('/users/login').send({
+      username: 'badTest',
+      password: 'test'
+    }).expect(/text-danger/).end(done);
   });
 
   it('connect to app with test user', (done) =>
@@ -45,6 +72,15 @@ describe('Users tests', function() {
     });
   });
 
+  it('test modify user page', async () =>
+  {
+    const account = await accounts.findOne({});
+
+    await server.get('/users/modifyUser')
+        .set('Cookie', `token=${account.token};`)
+        .expect(/Modifier l'utilisateur/).expect(/test/);
+  });
+
   it('modify user info (excluding password)', async () =>
   {
     const account = await accounts.findOne({});
@@ -61,8 +97,48 @@ describe('Users tests', function() {
     expect(account.username === modifiedAccount.username).toBe(false);
   });
 
+  it('modify user password', async () =>
+  {
+    const account = await accounts.findOne({});
+
+    await server.post('/users/modifyUser')
+        .set('Cookie', `token=${account.token};`)
+        .send({
+          username: 'testModify',
+          type: false,
+          password: 'test2',
+          oldPassword: 'test'
+        }).expect(/Enregistrement effectué/);
+  });
+
+  it('modify user password with bad oldPassword', async () =>
+  {
+    const account = await accounts.findOne({});
+
+    await server.post('/users/modifyUser')
+        .set('Cookie', `token=${account.token};`)
+        .send({
+          username: 'testModify',
+          type: false,
+          password: 'test3',
+          oldPassword: 'test'
+        }).expect(/text-danger/);
+  });
+
   it('access to unauthorized route when not connected', (done) =>
   {
     server.get('/users/logout').expect(302).expect('Location', '/').end(done);
+  });
+
+  it('logout', async () =>
+  {
+    const account = await accounts.findOne({});
+
+    await server.get('/users/logout')
+        .set('Cookie', `token=${account.token};`)
+        .expect(302);
+
+    const newAccount = await accounts.findOne({});
+    expect(newAccount.token).toBe("");
   });
 }); 
